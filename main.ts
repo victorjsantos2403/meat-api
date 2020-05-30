e2ecb5b651fcec4df48b05be9308e3a4d4ee6585
victor jesus <victor.santos@globalpoints.com.br>
2020-05-30 01:19:03 -0300

    implementacao inicial de exemplos com rest

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-console.log('main script 3...')
+import { usersRouter } from './users/users.router';
+import { Server } from './server/server';
+
+const server = new Server()
+server.bootstrap([usersRouter]).then(server => {
+    console.log('Server is listening on:', server.application.address());
+}).catch(error => {
+    console.log('Server failed to start');
+    console.error(error);
+    process.exit(1)
+})
+
